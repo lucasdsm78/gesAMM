e195ea7d45f056bdad081aa550740312b9d29270
chapelag <chapelag@domadj.fr>
2020-11-20 16:45:11 +0100

    nouvelle form

---
+++ gsb_gesAMM/frmNouvMed.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace gsb_gesAMM
+{
+    partial class frmNouvMed
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbFamille = new System.Windows.Forms.ComboBox();
+            this.tbContreIndic = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbCompo = new System.Windows.Forms.TextBox();
+            this.tbNomComm = new System.Windows.Forms.TextBox();
+            this.tbDepot = new System.Windows.Forms.TextBox();
+            this.tbEffets = new System.Windows.Forms.TextBox();
+            this.btAjout = new System.Windows.Forms.Button();
+            this.btQuit = new System.Windows.Forms.Button();
+            this.btNouv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dépôt légal :";
+            // 
+            // cbFamille
+            // 
+            this.cbFamille.FormattingEnabled = true;
+            this.cbFamille.Location = new System.Drawing.Point(271, 273);
+            this.cbFamille.Name = "cbFamille";
+            this.cbFamille.Size = new System.Drawing.Size(143, 24);
+            this.cbFamille.TabIndex = 1;
+            // 
+            // tbContreIndic
+            // 
+            this.tbContreIndic.Location = new System.Drawing.Point(271, 225);
+            this.tbContreIndic.Multiline = true;
+            this.tbContreIndic.Name = "tbContreIndic";
+            this.tbContreIndic.Size = new System.Drawing.Size(143, 22);
+            this.tbContreIndic.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nom commercial :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Composition :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(110, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Effets";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(110, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Contre indications :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(110, 280);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Famille :";
+            // 
+            // tbCompo
+            // 
+            this.tbCompo.Location = new System.Drawing.Point(271, 130);
+            this.tbCompo.Name = "tbCompo";
+            this.tbCompo.Size = new System.Drawing.Size(143, 22);
+            this.tbCompo.TabIndex = 8;
+            // 
+            // tbNomComm
+            // 
+            this.tbNomComm.Location = new System.Drawing.Point(271, 80);
+            this.tbNomComm.Name = "tbNomComm";
+            this.tbNomComm.Size = new System.Drawing.Size(143, 22);
+            this.tbNomComm.TabIndex = 9;
+            // 
+            // tbDepot
+            // 
+            this.tbDepot.Location = new System.Drawing.Point(271, 35);
+            this.tbDepot.Name = "tbDepot";
+            this.tbDepot.Size = new System.Drawing.Size(143, 22);
+            this.tbDepot.TabIndex = 10;
+            // 
+            // tbEffets
+            // 
+            this.tbEffets.Location = new System.Drawing.Point(271, 178);
+            this.tbEffets.Multiline = true;
+            this.tbEffets.Name = "tbEffets";
+            this.tbEffets.Size = new System.Drawing.Size(143, 22);
+            this.tbEffets.TabIndex = 11;
+            // 
+            // btAjout
+            // 
+            this.btAjout.Location = new System.Drawing.Point(467, 35);
+            this.btAjout.Name = "btAjout";
+            this.btAjout.Size = new System.Drawing.Size(106, 52);
+            this.btAjout.TabIndex = 12;
+            this.btAjout.Text = "Ajouter";
+            this.btAjout.UseVisualStyleBackColor = true;
+            // 
+            // btQuit
+            // 
+            this.btQuit.Location = new System.Drawing.Point(467, 212);
+            this.btQuit.Name = "btQuit";
+            this.btQuit.Size = new System.Drawing.Size(106, 53);
+            this.btQuit.TabIndex = 13;
+            this.btQuit.Text = "Quitter";
+            this.btQuit.UseVisualStyleBackColor = true;
+            this.btQuit.Click += new System.EventHandler(this.btQuit_Click);
+            // 
+            // btNouv
+            // 
+            this.btNouv.Location = new System.Drawing.Point(467, 130);
+            this.btNouv.Name = "btNouv";
+            this.btNouv.Size = new System.Drawing.Size(106, 48);
+            this.btNouv.TabIndex = 14;
+            this.btNouv.Text = "Nouveau";
+            this.btNouv.UseVisualStyleBackColor = true;
+            this.btNouv.Click += new System.EventHandler(this.btNouv_Click);
+            // 
+            // frmNouvMed
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 345);
+            this.Controls.Add(this.btNouv);
+            this.Controls.Add(this.btQuit);
+            this.Controls.Add(this.btAjout);
+            this.Controls.Add(this.tbEffets);
+            this.Controls.Add(this.tbDepot);
+            this.Controls.Add(this.tbNomComm);
+            this.Controls.Add(this.tbCompo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbContreIndic);
+            this.Controls.Add(this.cbFamille);
+            this.Controls.Add(this.label1);
+            this.Name = "frmNouvMed";
+            this.Text = "frmNouvMed";
+            this.Load += new System.EventHandler(this.frmNouvMed_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbFamille;
+        private System.Windows.Forms.TextBox tbContreIndic;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbCompo;
+        private System.Windows.Forms.TextBox tbNomComm;
+        private System.Windows.Forms.TextBox tbDepot;
+        private System.Windows.Forms.TextBox tbEffets;
+        private System.Windows.Forms.Button btAjout;
+        private System.Windows.Forms.Button btQuit;
+        private System.Windows.Forms.Button btNouv;
+    }
+}
---
+++ gsb_gesAMM/frmNouvMed.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gsb_gesAMM
+{
+    public partial class frmNouvMed : Form
+    {
+        public frmNouvMed()
+        {
+            InitializeComponent();
+        }
+
+        private void frmNouvMed_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btNouv_Click(object sender, EventArgs e)
+        {
+            tbDepot.Text = "";
+            tbNomComm.Text = "";
+            tbCompo.Text = "";
+            tbEffets.Text = "";
+            tbContreIndic.Text = "";
+            cbFamille.Text = "";
+        }
+    }
+}
