9e0bdaee19ff1dec8b49d39ca3002a110ee82547
lucasdsm78 <66834518+lucasdsm78@users.noreply.github.com>
2020-11-20 15:19:34 +0100

    Create Etape.cs

---
+++ gsb_gesAMM/Etape.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gsb_gesAMM
+{
+    class Etape
+    {
+        private int etp_num;
+        private string etp_libelle;
+        private string etp_norme;
+        private DateTime etp_date_norme;
+
+        public Etape(int leEtpNum, string leEtpLibelle, string leEtpNorme, DateTime leEtpDateNorme)
+        {
+            this.etp_num = leEtpNum;
+            this.etp_libelle = leEtpLibelle;
+            this.etp_norme = leEtpNorme;
+            this.etp_date_norme = leEtpDateNorme;
+        }
+
+        public int getEtpNum() { return this.etp_num; }
+        public string getEtpNorme() { return this.etp_norme; }
+        public string getEtpLibelle() { return this.etp_libelle; }
+        public DateTime getEtpDateNorme() { return this.etp_date_norme; }
+
+    }
+}
