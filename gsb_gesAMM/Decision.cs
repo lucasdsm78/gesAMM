39616f3874f5a627cb3b6ec740187a70c1bc5fbe
lucasdsm78 <66834518+lucasdsm78@users.noreply.github.com>
2020-11-20 15:10:54 +0100

    Class Decision

---
+++ gsb_gesAMM/Decision.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gsb_gesAMM
+{
+    class Decision
+    {
+        private int dcs_id;
+        private string dcs_libelle;
+
+        public Decision(int leDcsId, string leDcsLibelle)
+        {
+            this.dcs_id = leDcsId;
+            this.dcs_libelle = leDcsLibelle;
+        }
+
+        public int getDcsId() { return this.dcs_id; }
+        public string getDcsLibelle() { return this.dcs_libelle; }
+    }
+}
