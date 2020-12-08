59d37a07aed9a1771c856a87cd23291e8702a068
lucasdsm78 <66834518+lucasdsm78@users.noreply.github.com>
2020-12-08 05:05:27 +0100

    Avancement frmSaisieDecisionEtape

--- gsb_gesAMM/frmSaisieDecisionEtape.Designer.cs
+++ gsb_gesAMM/frmSaisieDecisionEtape.Designer.cs
@@ -41,33 +41,32 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gbProchaineEtape = new System.Windows.Forms.GroupBox();
             this.label10 = new System.Windows.Forms.Label();
             this.tbTypeDecision = new System.Windows.Forms.TextBox();
             this.tbDateDecision = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.btValider = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btValidDecision = new System.Windows.Forms.Button();
             this.gbDerniereEtape.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.gbProchaineEtape.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(108, 95);
+            this.label1.Location = new System.Drawing.Point(96, 76);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.Size = new System.Drawing.Size(84, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Medicament";
             // 
             // cbMedicament
             // 
             this.cbMedicament.FormattingEnabled = true;
-            this.cbMedicament.Location = new System.Drawing.Point(276, 91);
-            this.cbMedicament.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cbMedicament.Location = new System.Drawing.Point(245, 73);
             this.cbMedicament.Name = "cbMedicament";
-            this.cbMedicament.Size = new System.Drawing.Size(136, 28);
+            this.cbMedicament.Size = new System.Drawing.Size(121, 24);
             this.cbMedicament.TabIndex = 1;
             // 
             // gbDerniereEtape
@@ -82,195 +81,181 @@
             this.gbDerniereEtape.Controls.Add(this.label4);
             this.gbDerniereEtape.Controls.Add(this.label3);
             this.gbDerniereEtape.Controls.Add(this.label2);
-            this.gbDerniereEtape.Location = new System.Drawing.Point(111, 192);
-            this.gbDerniereEtape.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gbDerniereEtape.Location = new System.Drawing.Point(99, 154);
             this.gbDerniereEtape.Name = "gbDerniereEtape";
-            this.gbDerniereEtape.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.gbDerniereEtape.Size = new System.Drawing.Size(468, 355);
+            this.gbDerniereEtape.Size = new System.Drawing.Size(416, 284);
             this.gbDerniereEtape.TabIndex = 2;
             this.gbDerniereEtape.TabStop = false;
             this.gbDerniereEtape.Text = "Dernière Etape";
             // 
             // tbDateNorme
             // 
-            this.tbDateNorme.Location = new System.Drawing.Point(154, 302);
-            this.tbDateNorme.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbDateNorme.Location = new System.Drawing.Point(137, 242);
             this.tbDateNorme.Name = "tbDateNorme";
             this.tbDateNorme.ReadOnly = true;
-            this.tbDateNorme.Size = new System.Drawing.Size(112, 26);
+            this.tbDateNorme.Size = new System.Drawing.Size(100, 22);
             this.tbDateNorme.TabIndex = 13;
             // 
             // tbNorme
             // 
-            this.tbNorme.Location = new System.Drawing.Point(154, 246);
-            this.tbNorme.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbNorme.Location = new System.Drawing.Point(137, 197);
             this.tbNorme.Name = "tbNorme";
             this.tbNorme.ReadOnly = true;
-            this.tbNorme.Size = new System.Drawing.Size(112, 26);
+            this.tbNorme.Size = new System.Drawing.Size(100, 22);
             this.tbNorme.TabIndex = 12;
             // 
             // tbLibelle
             // 
-            this.tbLibelle.Location = new System.Drawing.Point(154, 182);
-            this.tbLibelle.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbLibelle.Location = new System.Drawing.Point(137, 146);
             this.tbLibelle.Name = "tbLibelle";
             this.tbLibelle.ReadOnly = true;
-            this.tbLibelle.Size = new System.Drawing.Size(112, 26);
+            this.tbLibelle.Size = new System.Drawing.Size(100, 22);
             this.tbLibelle.TabIndex = 11;
             // 
             // tbDate
             // 
-            this.tbDate.Location = new System.Drawing.Point(154, 125);
-            this.tbDate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbDate.Location = new System.Drawing.Point(137, 100);
             this.tbDate.Name = "tbDate";
             this.tbDate.ReadOnly = true;
-            this.tbDate.Size = new System.Drawing.Size(112, 26);
+            this.tbDate.Size = new System.Drawing.Size(100, 22);
             this.tbDate.TabIndex = 10;
             // 
             // tbNumero
             // 
-            this.tbNumero.Location = new System.Drawing.Point(154, 61);
-            this.tbNumero.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbNumero.Location = new System.Drawing.Point(137, 49);
             this.tbNumero.Name = "tbNumero";
             this.tbNumero.ReadOnly = true;
-            this.tbNumero.Size = new System.Drawing.Size(112, 26);
+            this.tbNumero.Size = new System.Drawing.Size(100, 22);
             this.tbNumero.TabIndex = 5;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(11, 306);
+            this.label6.Location = new System.Drawing.Point(10, 245);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(131, 20);
+            this.label6.Size = new System.Drawing.Size(117, 17);
             this.label6.TabIndex = 4;
             this.label6.Text = "Date de la norme";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(8, 246);
+            this.label5.Location = new System.Drawing.Point(7, 197);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(56, 20);
+            this.label5.Size = new System.Drawing.Size(50, 17);
             this.label5.TabIndex = 3;
             this.label5.Text = "Norme";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(11, 182);
+            this.label4.Location = new System.Drawing.Point(10, 146);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.Size = new System.Drawing.Size(49, 17);
             this.label4.TabIndex = 2;
             this.label4.Text = "Libelle";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 125);
+            this.label3.Location = new System.Drawing.Point(19, 100);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.Size = new System.Drawing.Size(38, 17);
             this.label3.TabIndex = 1;
             this.label3.Text = "Date";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(11, 65);
+            this.label2.Location = new System.Drawing.Point(10, 52);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.Size = new System.Drawing.Size(58, 17);
             this.label2.TabIndex = 0;
             this.label2.Text = "Numero";
             // 
-            // groupBox1
+            // gbProchaineEtape
             // 
-            this.groupBox1.Controls.Add(this.label10);
-            this.groupBox1.Controls.Add(this.tbTypeDecision);
-            this.groupBox1.Controls.Add(this.tbDateDecision);
-            this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Location = new System.Drawing.Point(658, 192);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox1.Size = new System.Drawing.Size(299, 231);
-            this.groupBox1.TabIndex = 14;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Prochaine Etape";
+            this.gbProchaineEtape.Controls.Add(this.label10);
+            this.gbProchaineEtape.Controls.Add(this.tbTypeDecision);
+            this.gbProchaineEtape.Controls.Add(this.tbDateDecision);
+            this.gbProchaineEtape.Controls.Add(this.label7);
+            this.gbProchaineEtape.Location = new System.Drawing.Point(585, 154);
+            this.gbProchaineEtape.Name = "gbProchaineEtape";
+            this.gbProchaineEtape.Size = new System.Drawing.Size(266, 185);
+            this.gbProchaineEtape.TabIndex = 14;
+            this.gbProchaineEtape.TabStop = false;
+            this.gbProchaineEtape.Text = "Prochaine Etape";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(0, 121);
+            this.label10.Location = new System.Drawing.Point(0, 97);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(105, 20);
+            this.label10.Size = new System.Drawing.Size(96, 17);
             this.label10.TabIndex = 15;
             this.label10.Text = "Type décision";
             // 
             // tbTypeDecision
             // 
-            this.tbTypeDecision.Location = new System.Drawing.Point(155, 118);
-            this.tbTypeDecision.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbTypeDecision.Location = new System.Drawing.Point(138, 94);
             this.tbTypeDecision.Name = "tbTypeDecision";
-            this.tbTypeDecision.Size = new System.Drawing.Size(112, 26);
+            this.tbTypeDecision.Size = new System.Drawing.Size(100, 22);
             this.tbTypeDecision.TabIndex = 14;
             // 
             // tbDateDecision
             // 
-            this.tbDateDecision.Location = new System.Drawing.Point(155, 61);
-            this.tbDateDecision.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbDateDecision.Location = new System.Drawing.Point(138, 49);
             this.tbDateDecision.Name = "tbDateDecision";
-            this.tbDateDecision.Size = new System.Drawing.Size(112, 26);
+            this.tbDateDecision.Size = new System.Drawing.Size(100, 22);
             this.tbDateDecision.TabIndex = 13;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(3, 65);
+            this.label7.Location = new System.Drawing.Point(3, 52);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(144, 20);
+            this.label7.Size = new System.Drawing.Size(129, 17);
             this.label7.TabIndex = 4;
             this.label7.Text = "Date de la décision";
             // 
             // btValider
             // 
-            this.btValider.Location = new System.Drawing.Point(456, 70);
-            this.btValider.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btValider.Location = new System.Drawing.Point(405, 56);
             this.btValider.Name = "btValider";
-            this.btValider.Size = new System.Drawing.Size(168, 70);
+            this.btValider.Size = new System.Drawing.Size(149, 56);
             this.btValider.TabIndex = 15;
             this.btValider.Text = "Valider le médicament";
             this.btValider.UseVisualStyleBackColor = true;
             this.btValider.Click += new System.EventHandler(this.btValider_Click);
             // 
-            // button1
+            // btValidDecision
             // 
-            this.button1.Location = new System.Drawing.Point(702, 476);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(176, 80);
-            this.button1.TabIndex = 16;
-            this.button1.Text = "Valider la décision";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btValidDecision.Location = new System.Drawing.Point(624, 381);
+            this.btValidDecision.Name = "btValidDecision";
+            this.btValidDecision.Size = new System.Drawing.Size(156, 64);
+            this.btValidDecision.TabIndex = 16;
+            this.btValidDecision.Text = "Valider la décision";
+            this.btValidDecision.UseVisualStyleBackColor = true;
             // 
             // frmSaisieDecisionEtape
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btValidDecision);
             this.Controls.Add(this.btValider);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbProchaineEtape);
             this.Controls.Add(this.gbDerniereEtape);
             this.Controls.Add(this.cbMedicament);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmSaisieDecisionEtape";
             this.Text = "Saisir une date de décision d\'une étape";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmSaisieDecisionEtape_Load);
             this.gbDerniereEtape.ResumeLayout(false);
             this.gbDerniereEtape.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.gbProchaineEtape.ResumeLayout(false);
+            this.gbProchaineEtape.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -291,12 +276,12 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox gbProchaineEtape;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.TextBox tbTypeDecision;
         private System.Windows.Forms.TextBox tbDateDecision;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button btValider;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btValidDecision;
     }
 }
--- gsb_gesAMM/frmSaisieDecisionEtape.cs
+++ gsb_gesAMM/frmSaisieDecisionEtape.cs
@@ -19,7 +19,53 @@ namespace gsb_gesAMM
 
         private void btValider_Click(object sender, EventArgs e)
         {
+            List<DateTime> lesDatesDuDepotLegal = new List<DateTime>();
+            int decisionId = 0;
+            int etpNum = 0;
+            string medId = "";
+            DateTime max = DateTime.MinValue;
+            string depotlegalchoisi = cbMedicament.Text;
+            foreach(string laReference in Globale.lesMedicaments.Keys)
+            {
+                Medicament unMedicament = Globale.lesMedicaments[laReference];
+                foreach(WorkFlow leWorkflow in unMedicament.getLesEtapes())
+                {
+                    if(leWorkflow.getWkfMedId() == depotlegalchoisi)
+                    {
+                        if(leWorkflow.getWkfDateDecision() > max)
+                        {
+                            max = leWorkflow.getWkfDateDecision();
+                            decisionId = leWorkflow.getWkfDcsId();
+                            etpNum = leWorkflow.getWkfEtpNum();
+                            medId = leWorkflow.getWkfMedId();
+                        }
+                    }
+                }
+            }
+            if(decisionId == 1)
+            {
+                MessageBox.Show("la dernière étape est acceptée", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
+                gbDerniereEtape.Visible = true;
+                gbProchaineEtape.Visible = true;
+                btValidDecision.Visible = true;
+
+                foreach(Etape uneEtape in Globale.lesEtapes)
+                {
+                    if(uneEtape.getEtpNum() == etpNum)
+                    {
+                        tbDate.Text = max.ToShortDateString();
+                        tbDateNorme.Text = uneEtape.getEtpDateNorme().ToShortDateString();
+                        tbLibelle.Text = uneEtape.getEtpLibelle();
+                        tbNorme.Text = uneEtape.getEtpNorme();
+                        tbNumero.Text = etpNum.ToString();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("La dernière étape est refusée", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void chargerListeMedicament()
@@ -36,6 +82,9 @@ namespace gsb_gesAMM
         private void frmSaisieDecisionEtape_Load(object sender, EventArgs e)
         {
             chargerListeMedicament();
+            gbDerniereEtape.Visible = false;
+            gbProchaineEtape.Visible = false;
+            btValidDecision.Visible = false;
         }
     }
 }
