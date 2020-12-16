f981c44c97f0ff7b95c3fb97937c82e39271fd63
Bryx <33637996+bryx@users.noreply.github.com>
2020-12-16 18:21:49 +0100

    MAJ form bryan

--- gsb_gesAMM/frmMedAuto.Designer.cs
+++ gsb_gesAMM/frmMedAuto.Designer.cs
@@ -50,28 +50,30 @@
             this.columnHeader3});
             this.lvListMed.FullRowSelect = true;
             this.lvListMed.HideSelection = false;
-            this.lvListMed.Location = new System.Drawing.Point(18, 16);
-            this.lvListMed.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lvListMed.Location = new System.Drawing.Point(20, 14);
+            this.lvListMed.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lvListMed.MultiSelect = false;
             this.lvListMed.Name = "lvListMed";
-            this.lvListMed.Size = new System.Drawing.Size(1032, 206);
+            this.lvListMed.Size = new System.Drawing.Size(1160, 256);
             this.lvListMed.TabIndex = 0;
             this.lvListMed.UseCompatibleStateImageBehavior = false;
             this.lvListMed.View = System.Windows.Forms.View.Details;
+            this.lvListMed.SelectedIndexChanged += new System.EventHandler(this.lvListMed_SelectedIndexChanged);
             // 
             // columnHeader1
             // 
             this.columnHeader1.Text = "Code famille";
-            this.columnHeader1.Width = 120;
+            this.columnHeader1.Width = 311;
             // 
             // columnHeader2
             // 
             this.columnHeader2.Text = "Designation famille";
-            this.columnHeader2.Width = 150;
+            this.columnHeader2.Width = 400;
             // 
             // columnHeader3
             // 
             this.columnHeader3.Text = "NB médicaments autorisés";
-            this.columnHeader3.Width = 150;
+            this.columnHeader3.Width = 239;
             // 
             // lvListMedAuto
             // 
@@ -83,10 +85,10 @@
             this.columnHeader8,
             this.columnHeader9});
             this.lvListMedAuto.HideSelection = false;
-            this.lvListMedAuto.Location = new System.Drawing.Point(18, 230);
-            this.lvListMedAuto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lvListMedAuto.Location = new System.Drawing.Point(20, 288);
+            this.lvListMedAuto.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.lvListMedAuto.Name = "lvListMedAuto";
-            this.lvListMedAuto.Size = new System.Drawing.Size(1032, 253);
+            this.lvListMedAuto.Size = new System.Drawing.Size(1160, 315);
             this.lvListMedAuto.TabIndex = 1;
             this.lvListMedAuto.UseCompatibleStateImageBehavior = false;
             this.lvListMedAuto.View = System.Windows.Forms.View.Details;
@@ -123,10 +125,10 @@
             // 
             // btQuitter
             // 
-            this.btQuitter.Location = new System.Drawing.Point(389, 498);
-            this.btQuitter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btQuitter.Location = new System.Drawing.Point(438, 622);
+            this.btQuitter.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btQuitter.Name = "btQuitter";
-            this.btQuitter.Size = new System.Drawing.Size(249, 41);
+            this.btQuitter.Size = new System.Drawing.Size(280, 51);
             this.btQuitter.TabIndex = 4;
             this.btQuitter.Text = "Quitter";
             this.btQuitter.UseVisualStyleBackColor = true;
@@ -134,13 +136,13 @@
             // 
             // frmMedAuto
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.btQuitter);
             this.Controls.Add(this.lvListMedAuto);
             this.Controls.Add(this.lvListMed);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmMedAuto";
             this.Text = "Consulter médicaments autorisés";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
--- gsb_gesAMM/frmMedAuto.cs
+++ gsb_gesAMM/frmMedAuto.cs
@@ -24,6 +24,8 @@ namespace gsb_gesAMM
 
         private void frmMedAuto_Load(object sender, EventArgs e)
         {
+            changerListe = false;
+
             lvListMed.Items.Clear();
 
             foreach (string uneFamille in Globale.lesFamilles.Keys)
@@ -48,6 +50,36 @@ namespace gsb_gesAMM
                 ligne.SubItems.Add(nbMedicament.ToString());
 
                 lvListMed.Items.Add(ligne);
+            }
+
+            changerListe = true;
+        }
+
+        Boolean changerListe;
+
+        private void lvListMed_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (changerListe == true && lvListMed.SelectedItems.Count > 0)
+            {
+                lvListMedAuto.Items.Clear();
+
+                foreach (string leDepotLegal in Globale.lesMedicaments.Keys)
+                {
+                    Medicament unMedicament = Globale.lesMedicaments[leDepotLegal];
+
+                    if (unMedicament.getMedCodeFamille() == lvListMed.SelectedItems[0].Text)
+                    {
+                        ListViewItem ligne = new ListViewItem();
+                        ligne.Text = unMedicament.getMedDepotLegal();
+                        ligne.SubItems.Add(unMedicament.getMedNomCommercial());
+                        ligne.SubItems.Add(unMedicament.getMedComposition());
+                        ligne.SubItems.Add(unMedicament.getMedEffets());
+                        ligne.SubItems.Add(unMedicament.getMedContreIndications());
+                        ligne.SubItems.Add(unMedicament.getMedAmm());
+
+                        lvListMedAuto.Items.Add(ligne);
+                    }
+                }
             }
         }
     }
